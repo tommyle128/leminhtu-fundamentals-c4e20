23cefa3b4f0523330c538de2247dfe5cf44300e5
tommyle128 <minhtu.le128@gmail.com>
2018-07-27 19:44:04 +0700

    intital commit

--- Session03/homework/2.6.py
+++ Session03/homework/2.6.py
@@ -1,6 +1,8 @@
 flock = [5, 7, 300, 90, 24, 50, 75]
 print("Hello, my name is Hiep and there are my sheep sizes:")
 print(flock)
+
+print()
 highest = max(flock)
 print("Now my biggest sheep has size:", highest,". Let's shear it!")
 for index, item in enumerate(flock):
